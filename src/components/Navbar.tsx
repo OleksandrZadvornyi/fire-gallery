9577f9fcf28e500b994d883f311d5224edf493de
Oleksandr Zadvornyi <zadvornyi.alex16@gmail.com>
2023-08-17 15:19:52 +0300

    add home page

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+const Navbar = () => {
+  return (
+    <div className="navbar bg-base-100 justify-between">
+      <a className="font-bold normal-case text-xl underline">FireGallery 📷</a>
+      <button>Logout</button>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,13 @@
+import ImageGallery from "../components/ImageGallery";
+import Navbar from "../components/Navbar";
+import UploadForm from "../components/UploadForm";
+
 const Home = () => {
-    return <div>Home</div>
+    return <div className="max-w-4xl mx-auto">
+        <Navbar />
+        <UploadForm />
+        <ImageGallery />
+    </div>
 };
 
 export default Home;
