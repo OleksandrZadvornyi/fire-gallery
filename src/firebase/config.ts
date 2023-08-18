5a18c00eda121a7abb260debe365a86bfe510441
Oleksandr Zadvornyi <zadvornyi.alex16@gmail.com>
2023-08-18 20:40:40 +0300

    add firebase connection and routes

---
+++ src/firebase/config.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDeDryXYoN5Ut65ezbjm0NKgsc55cVNd5w",
+  authDomain: "fire-gallery-84947.firebaseapp.com",
+  projectId: "fire-gallery-84947",
+  storageBucket: "fire-gallery-84947.appspot.com",
+  messagingSenderId: "742252252251",
+  appId: "1:742252252251:web:b1b4efac1d34a882cf837e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
