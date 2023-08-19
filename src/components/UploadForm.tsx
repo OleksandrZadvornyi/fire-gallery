5f6773bf8cbf668fcf2f78889bb068d8b7ed88f6
Oleksandr Zadvornyi <zadvornyi.alex16@gmail.com>
2023-08-19 17:35:46 +0300

    add an ability to upload images to firestore DB

--- src/components/UploadForm.tsx
+++ src/components/UploadForm.tsx
@@ -1,12 +1,43 @@
+import { useState } from "react";
+import useStorage from "../hooks/useStorage";
+
 const UploadForm = () => {
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const { startUpload, progress } = useStorage();
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files[0]) {
+      setSelectedFile(e.target.files[0]);
+    }
+  };
+
+  const handleSubmitFunction = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (selectedFile) {
+      // upload image
+      startUpload(selectedFile);
+    }
+    setSelectedFile(null);
+  };
+
   return (
     <div className="text-center mt-10">
-      <form className="flex items-center flex-col gap-8">
+      <form
+        onSubmit={handleSubmitFunction}
+        className="flex items-center flex-col gap-8"
+      >
         <input
           type="file"
+          onChange={handleFileChange}
           className="file-input file-input-bordered w-full max-w-xs"
         />
-        <button className="btn gap-3">Upload <span>🚀</span></button>
+        <button
+          type="submit"
+          className={`btn gap-3 ${Boolean(progress) && "loading"}`}
+          disabled={!selectedFile}
+        >
+          Upload <span>🚀</span>
+        </button>
       </form>
     </div>
   );
---
+++ src/hooks/useStorage.ts
@@ -0,0 +1,57 @@
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+import { useState } from "react";
+import { storage } from "../firebase/config";
+import { v4 as uuidv4 } from "uuid";
+
+const useStorage = () => {
+  const [progress, setProgress] = useState<number>(0);
+  const [error, setError] = useState<Error | null>(null);
+  const [url, setUrl] = useState<string | null>(null);
+
+  const startUpload = (file: File) => {
+    if (!file) {
+      return;
+    }
+
+    const fileId = uuidv4();
+    const formatFile = file.type.split("/")[1];
+
+    const storageRef = ref(storage, `images/${fileId}.${formatFile}`);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        // Observe state change events such as progress, pause, and resume
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log("Upload is " + progress + "% done");
+        switch (snapshot.state) {
+          case "paused":
+            console.log("Upload is paused");
+            break;
+          case "running":
+            console.log("Upload is running");
+            break;
+        }
+        setProgress(progress);
+      },
+      (error) => {
+        setError(error);
+      },
+      () => {
+        // Handle successful uploads on complete
+        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setUrl(downloadURL);
+          setProgress(progress);
+        });
+      }
+    );
+  };
+
+  return { progress, error, startUpload };
+};
+
+export default useStorage;
